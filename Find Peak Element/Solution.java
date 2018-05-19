c963dd2d8a530208fdebb62ac1a077665fc253fd
Rekhansh Panchal <rekhanshpanchal@gmail.com>
2018-05-19 18:00:57 -0400

    Create Solution.java

---
+++ Find Peak Element/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+    
+        if (nums.length == 1)
+            return 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1])
+                return i;
+        }
+        return nums.length - 1;
+    }
+}
